83d6bc6cdcebdc707fdd92ca13d8d83ef3ea005d
Shums Kassam <skassam21@gmail.com>
2017-10-29 17:33:46 -0400

    reformat everything

--- client.c
+++ client.c
@@ -3,7 +3,7 @@
 
    Author: Shums Kassam
    Sources: http://www.linuxjournal.com/article/4822
-*/
+ */
 
 #include <unistd.h>
 #include <string.h>
@@ -33,43 +33,27 @@
 #define CA_LIST "568ca.pem"
 #define COMMON_NAME "Bob's Server"
 #define EMAIL_ADDRESS "ece568bob@ecf.utoronto.ca"
+#define ALLOWED_CIPHERS "SHA1"
 
-BIO *bio_err=0;
+BIO *bio_err = 0;
 static char *pass;
-const char *allowedCiphers = "SHA1";
 
-static int password_cb(char *buf,int num,
-  int rwflag,void *userdata)
-  {
-    if(num<strlen(pass)+1)
-      return(0);
-
-    strcpy(buf,pass);
-    return(strlen(pass));
-  }
-
-void destroy_ctx(SSL_CTX* ctx)
+/* Print an error and exit */
+int error_exit(char *string)
 {
-  SSL_CTX_free(ctx);
+  fprintf(stderr, "ECE568-CLIENT: %s\n", string);
+  exit(0);
 }
 
-/* A simple error and exit routine*/
-int err_exit(string)
-  char *string;
-  {
-    fprintf(stderr,"ECE568-CLIENT: %s\n",string);
-    exit(0);
-  }
+/* Print SSL errors and exit */
+int bio_error_exit(char *string)
+{
+  BIO_printf(bio_err, "ECE568-CLIENT: %s\n", string);
+  ERR_print_errors(bio_err);
+  exit(0);
+}
 
-/* Print SSL errors and exit*/
-int berr_exit(char *string)
-  {
-    BIO_printf(bio_err,"ECE568-CLIENT: %s\n",string);
-    ERR_print_errors(bio_err);
-    exit(0);
-  }
-
-/* Check that the server certificate is valid*/
+/* Check that the server certificate is valid */
 void check_cert(SSL *ssl)
 {
   X509 *peer;
@@ -78,10 +62,9 @@ void check_cert(SSL *ssl)
   char issuer[256];
 
   if(SSL_get_verify_result(ssl)!=X509_V_OK)
-    berr_exit(FMT_NO_VERIFY);
+    bio_error_exit(FMT_NO_VERIFY);
 
-  /*Check the common name*/
-  peer=SSL_get_peer_certificate(ssl);
+  peer = SSL_get_peer_certificate(ssl);
 
   X509_NAME_get_text_by_NID(
     X509_get_issuer_name(peer), NID_commonName, issuer, 256);
@@ -92,13 +75,14 @@ void check_cert(SSL *ssl)
   X509_NAME_get_text_by_NID(
     X509_get_subject_name(peer), NID_pkcs9_emailAddress, peer_email_address, 256);
 
+  /*Check the common name*/
   if(strcasecmp(peer_CN,COMMON_NAME)) {
-    err_exit
-      (FMT_CN_MISMATCH);
+    error_exit(FMT_CN_MISMATCH);
   }
+
+  /*Check the email address*/
   if(strcasecmp(peer_email_address,EMAIL_ADDRESS)) {
-    err_exit
-      (FMT_EMAIL_MISMATCH);
+    error_exit(FMT_EMAIL_MISMATCH);
   }
 
   // Certificate is valid
@@ -109,68 +93,67 @@ void check_cert(SSL *ssl)
 static void sigpipe_handle(int x){
 }
 
-SSL_CTX *initialize_ctx(char *keyfile, char* password)
+static int password_callback(char *buf, int num,
+                             int rwflag, void *userdata)
 {
-    const SSL_METHOD *meth;
-    SSL_CTX *ctx;
+  if(num < strlen(pass) + 1)
+    return(0);
 
-    if(!bio_err){
-      /* Global system initialization*/
-      SSL_library_init();
-      SSL_load_error_strings();
+  strcpy(buf, pass);
+  return(strlen(pass));
+}
 
-      /* An error write context */
-      bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
-    }
+SSL_CTX *initialize_ctx(char *keyfile, char *password)
+{
+  const SSL_METHOD *meth;
+  SSL_CTX *ctx;
 
-    /* Set up a SIGPIPE handler */
-    signal(SIGPIPE,sigpipe_handle);
-
-    /* Create our context*/
-    meth=SSLv23_client_method();
-    ctx=SSL_CTX_new(meth);
-    SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv3);
-    SSL_CTX_set_cipher_list(ctx, allowedCiphers);
-
-    /* Load our keys and certificates*/
-    if(!(SSL_CTX_use_certificate_chain_file(ctx,
-      keyfile)))
-      berr_exit("Can't read certificate file");
-
-    pass=password;
-    SSL_CTX_set_default_passwd_cb(ctx,
-      password_cb);
-    if(!(SSL_CTX_use_PrivateKey_file(ctx,
-      keyfile,SSL_FILETYPE_PEM)))
-      berr_exit("Can't read key file");
-
-    /* Load the CAs we trust*/
-    if(!(SSL_CTX_load_verify_locations(ctx,
-      CA_LIST,0)))
-      berr_exit("Can't read CA list");
-
-    return ctx;
+  if(!bio_err) {
+    SSL_library_init();
+    SSL_load_error_strings();
+    bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
   }
+
+  signal(SIGPIPE,sigpipe_handle);
+
+  /* Create ctx */
+  meth=SSLv23_client_method();
+  ctx=SSL_CTX_new(meth);
+
+  /* Use only SSLv3 or TLSv1 (exclude SSLv2) */
+  SSL_CTX_set_options(ctx, SSL_OP_NO_SSLv2);
+
+  /* Only communicate with SHA1 server */
+  SSL_CTX_set_cipher_list(ctx, ALLOWED_CIPHERS);
+
+  /* Load keys and certificates */
+  if(!(SSL_CTX_use_certificate_chain_file(ctx,
+                                          keyfile)))
+    bio_error_exit("Can't read certificate file");
+
+  pass=password;
+  SSL_CTX_set_default_passwd_cb(ctx,
+                                password_callback);
+  if(!(SSL_CTX_use_PrivateKey_file(ctx,
+                                   keyfile,SSL_FILETYPE_PEM)))
+    bio_error_exit("Can't read key file");
+
+  /* Load the trusted certificates */
+  if(!(SSL_CTX_load_verify_locations(ctx,
+                                     CA_LIST,0)))
+    bio_error_exit("Can't read CA list");
+
+  return ctx;
+}
 
 void shutdown_client(SSL* ssl) {
   int r;
-  r=SSL_shutdown(ssl);
-  if(!r){
-    /* If we called SSL_shutdown() first then
-       we always get return value of '0'. In
-       this case, try again, but first send a
-       TCP FIN to trigger the other side's
-       close_notify*/
+  r = SSL_shutdown(ssl);
+  if(!r) {
     r=SSL_shutdown(ssl);
   }
-
-  switch(r){
-    case 1:
-      break; /* Success */
-    case 0:
-    case -1:
-    default:
-      berr_exit(FMT_INCORRECT_CLOSE);
+  if (r != 1) {
+    bio_error_exit(FMT_INCORRECT_CLOSE);          
   }
 }
 
@@ -178,7 +161,7 @@ int main(int argc, char **argv)
 {
   int len, sock, port=PORT;
   int r;
-  char *host=HOST;
+  char *host = HOST;
   SSL_CTX *ctx;
   SSL *ssl;
   BIO *sbio;
@@ -188,28 +171,26 @@ int main(int argc, char **argv)
   char *secret = "What's the question?";
 
   /*Parse command line arguments*/
-
-  switch(argc){
-    case 1:
-      break;
-    case 3:
-      host = argv[1];
-      port=atoi(argv[2]);
-      if (port<1||port>65535){
-	fprintf(stderr,"invalid port number");
-	exit(0);
-      }
-      break;
-    default:
-      printf("Usage: %s server port\n", argv[0]);
+  switch(argc) {
+  case 1:
+    break;
+  case 3:
+    host = argv[1];
+    port=atoi(argv[2]);
+    if (port<1||port>65535) {
+      fprintf(stderr,"invalid port number");
       exit(0);
+    }
+    break;
+  default:
+    printf("Usage: %s server port\n", argv[0]);
+    exit(0);
   }
 
   /*get ip address of the host*/
-
   host_entry = gethostbyname(host);
 
-  if (!host_entry){
+  if (!host_entry) {
     fprintf(stderr,"Couldn't resolve host");
     exit(0);
   }
@@ -221,8 +202,8 @@ int main(int argc, char **argv)
 
   printf("Connecting to %s(%s):%d\n", host, inet_ntoa(addr.sin_addr),port);
 
-  /* Build our SSL context*/
-  ctx=initialize_ctx(KEYFILE,PASSWORD);
+  /* Initialize SSL context */
+  ctx=initialize_ctx(KEYFILE, PASSWORD);
 
   /*open socket*/
   if((sock=socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))<0)
@@ -231,42 +212,48 @@ int main(int argc, char **argv)
     perror("connect");
 
   /* Connect the SSL socket */
-  ssl=SSL_new(ctx);
-  sbio=BIO_new_socket(sock,BIO_NOCLOSE);
-  SSL_set_bio(ssl,sbio,sbio);
-  if(SSL_connect(ssl)<=0)
-      berr_exit("SSL connect error");
+  ssl = SSL_new(ctx);
+  sbio = BIO_new_socket(sock, BIO_NOCLOSE);
+  SSL_set_bio(ssl, sbio, sbio);
+  if(SSL_connect(ssl) <= 0)
+    bio_error_exit("SSL connect error");
+  
+  /* check the certificate */  
   check_cert(ssl);
 
   /* send the secret question across */
   r=SSL_write(ssl, secret, strlen(secret));
-  switch(SSL_get_error(ssl,r)){
+
+  switch(SSL_get_error(ssl,r)) {
   case SSL_ERROR_NONE:
     if(strlen(secret)!=r)
-      err_exit("Incomplete write");
+      error_exit("Incomplete write");
     break;
-    default:
-      berr_exit("SSL write problem");
+  default:
+    bio_error_exit("SSL write problem");
   }
+
+  /* read the secret answer */
   r = SSL_read(ssl, &buf, 255);
-  switch(SSL_get_error(ssl,r)){
-    case SSL_ERROR_NONE:
-      len=r;
-      buf[len]= '\0';
+  switch(SSL_get_error(ssl,r)) {
+  case SSL_ERROR_NONE:
+    len=r;
+    buf[len]= '\0';
 
-      printf(FMT_OUTPUT, secret, buf);
-      break;
-    case SSL_ERROR_ZERO_RETURN:
-      break;
-    case SSL_ERROR_SYSCALL:
-      err_exit(FMT_INCORRECT_CLOSE);
-    default:
-      berr_exit("SSL read problem");
+    printf(FMT_OUTPUT, secret, buf);
+    break;
+  case SSL_ERROR_ZERO_RETURN:
+    break;
+  case SSL_ERROR_SYSCALL:
+    error_exit(FMT_INCORRECT_CLOSE);
+  default:
+    bio_error_exit("SSL read problem");
   }
 
+  /* shutdown client and destroy ctx */
   shutdown_client(ssl);
   SSL_free(ssl);
-  destroy_ctx(ctx);
+  SSL_CTX_free(ctx);
   close(sock);
 
   return 1;
--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 
    Author: Shums Kassam
    Sources: http://www.linuxjournal.com/article/4822
-*/
+ */
 
 #include <unistd.h>
 #include <string.h>
@@ -27,37 +27,28 @@
 #define KEYFILE "bob.pem"
 #define PASSWORD "password"
 #define CA_LIST "568ca.pem"
+#define ALLOWED_CIPHERS "ALL"
+
 
 BIO *bio_err = 0;
 static char *pass;
-const char *allowedCiphers = "ALL";
 
-static int password_cb(char *buf,int num,
-  int rwflag,void *userdata)
-  {
-    if(num<strlen(pass)+1)
-      return(0);
-
-    strcpy(buf,pass);
-    return(strlen(pass));
-  }
-
-/* A simple error and exit routine*/
-int err_exit(char* string)
+/* Print an error and exit */
+int error_exit(char *string)
 {
   fprintf(stderr,"ECE568-SERVER: %s\n",string);
   exit(0);
 }
 
-/* Print SSL errors and exit*/
-int berr_exit(char* string)
+/* Print SSL errors and exit */
+int bio_error_exit(char *string)
 {
   BIO_printf(bio_err,"ECE568-SERVER: %s\n",string);
   ERR_print_errors(bio_err);
   exit(0);
 }
 
-/* Check that the server certificate is valid*/
+/* Check that the client certificate is valid */
 void check_cert(SSL *ssl)
 {
   X509 *peer;
@@ -66,8 +57,9 @@ void check_cert(SSL *ssl)
 
   peer=SSL_get_peer_certificate(ssl);
 
+  /* Return error if client certificate doesn't exist or the certificate is invalid */
   if(SSL_get_verify_result(ssl)!=X509_V_OK || !peer)
-    berr_exit("SSL accept error");
+    bio_error_exit("SSL accept error");
 
   X509_NAME_get_text_by_NID(
     X509_get_subject_name(peer), NID_commonName, peer_CN, 256);
@@ -75,88 +67,80 @@ void check_cert(SSL *ssl)
   X509_NAME_get_text_by_NID(
     X509_get_subject_name(peer), NID_pkcs9_emailAddress, peer_email_address, 256);
 
+  // Print out the certificate's CN and email address
   printf(FMT_CLIENT_INFO, peer_CN, peer_email_address);
-
 }
 
 static void sigpipe_handle(int x){
 }
 
-void destroy_ctx(SSL_CTX* ctx)
+static int password_callback(char *buf, int num,
+                             int rwflag, void *userdata)
 {
-  SSL_CTX_free(ctx);
+  if(num < strlen(pass) + 1)
+    return(0);
+
+  strcpy(buf, pass);
+  return(strlen(pass));
 }
 
 SSL_CTX *initialize_ctx(char *keyfile, char* password)
 {
-    const SSL_METHOD *meth;
-    SSL_CTX *ctx;
+  const SSL_METHOD *meth;
+  SSL_CTX *ctx;
 
-    if(!bio_err){
-      /* Global system initialization*/
-      SSL_library_init();
-      SSL_load_error_strings();
-
-      /* An error write context */
-      bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
-    }
-
-    /* Set up a SIGPIPE handler */
-    signal(SIGPIPE,sigpipe_handle);
-
-    /* Create our context*/
-    meth=SSLv23_server_method();
-    ctx=SSL_CTX_new(meth);
-    SSL_CTX_set_cipher_list(ctx, allowedCiphers);
-
-    /* Load our keys and certificates*/
-    if(!(SSL_CTX_use_certificate_chain_file(ctx,
-      keyfile)))
-      berr_exit("Can't read certificate file");
-
-    pass=password;
-    SSL_CTX_set_default_passwd_cb(ctx,
-      password_cb);
-    if(!(SSL_CTX_use_PrivateKey_file(ctx,
-      keyfile,SSL_FILETYPE_PEM)))
-      berr_exit("Can't read key file");
-
-    /* Load the CAs we trust*/
-    if(!(SSL_CTX_load_verify_locations(ctx,
-      CA_LIST,0)))
-      berr_exit("Can't read CA list");
-
-    // Have the server send the CA list to client to verify client
-    // certificates
-    SSL_CTX_set_client_CA_list(ctx, SSL_load_client_CA_file(CA_LIST));
-    SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER|SSL_VERIFY_FAIL_IF_NO_PEER_CERT,
-      NULL);
-
-    return ctx;
+  if(!bio_err) {
+    SSL_library_init();
+    SSL_load_error_strings();
+    bio_err=BIO_new_fp(stderr,BIO_NOCLOSE);
   }
+
+  signal(SIGPIPE,sigpipe_handle);
+
+  /* Create ctx */
+  /* Use SSLv2, SSLv3 and TLSv1 */
+  meth=SSLv23_server_method();
+  ctx=SSL_CTX_new(meth);
+
+  /* Allow use of ALL types of ciphers */
+  SSL_CTX_set_cipher_list(ctx, ALLOWED_CIPHERS);
+
+  /* Load keys and certificates */
+  if(!(SSL_CTX_use_certificate_chain_file(ctx,
+                                          keyfile)))
+    bio_error_exit("Can't read certificate file");
+
+  pass=password;
+  SSL_CTX_set_default_passwd_cb(ctx,
+                                password_callback);
+  if(!(SSL_CTX_use_PrivateKey_file(ctx,
+                                   keyfile,SSL_FILETYPE_PEM)))
+    bio_error_exit("Can't read key file");
+
+  /* Load the trusted certificates */
+  if(!(SSL_CTX_load_verify_locations(ctx,
+                                     CA_LIST,0)))
+    bio_error_exit("Can't read CA list");
+
+  // Have the server send the CA list to client to verify client certificates
+  SSL_CTX_set_client_CA_list(ctx, SSL_load_client_CA_file(CA_LIST));
+  SSL_CTX_set_verify(ctx, SSL_VERIFY_PEER|SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+
+  return ctx;
+}
 
 
 void shutdown_server(SSL* ssl, int s) {
   int r;
 
   r=SSL_shutdown(ssl);
-  if(!r){
-    /* If we called SSL_shutdown() first then
-       we always get return value of '0'. In
-       this case, try again, but first send a
-       TCP FIN to trigger the other side's
-       close_notify*/
+  if(!r) {
     shutdown(s,1);
     r=SSL_shutdown(ssl);
   }
 
-  switch(r) {
-    case 1:
-      break; /* Success */
-    case 0:
-    case -1:
-    default:
-      berr_exit(FMT_INCOMPLETE_CLOSE);
+  if (r != 1) {
+    bio_error_exit(FMT_INCOMPLETE_CLOSE);    
   }
 }
 
@@ -172,27 +156,26 @@ int main(int argc, char **argv)
   SSL *ssl;
   int r;
 
-  /* Build our SSL context*/
-  ctx=initialize_ctx(KEYFILE,PASSWORD);
+  /* Initialize SSL context */
+  ctx=initialize_ctx(KEYFILE, PASSWORD);
 
   /*Parse command line arguments*/
-
-  switch(argc){
-    case 1:
-      break;
-    case 2:
-      port=atoi(argv[1]);
-      if (port<1||port>65535){
-	fprintf(stderr,"invalid port number");
-	exit(0);
-      }
-      break;
-    default:
-      printf("Usage: %s port\n", argv[0]);
-      exit(0);
+  switch(argc) {
+  case 1:
+    break;
+  case 2:
+    port=atoi(argv[1]);
+    if (port<1||port>65535) {
+            fprintf(stderr,"invalid port number");
+            exit(0);
+    }
+    break;
+  default:
+    printf("Usage: %s port\n", argv[0]);
+    exit(0);
   }
 
-  if((sock=socket(AF_INET,SOCK_STREAM,0))<0){
+  if((sock=socket(AF_INET,SOCK_STREAM,0))<0) {
     perror("socket");
     close(sock);
     exit(0);
@@ -205,31 +188,31 @@ int main(int argc, char **argv)
 
   setsockopt(sock,SOL_SOCKET,SO_REUSEADDR, &val,sizeof(val));
 
-  if(bind(sock,(struct sockaddr *)&sin, sizeof(sin))<0){
+  if(bind(sock,(struct sockaddr *)&sin, sizeof(sin))<0) {
     perror("bind");
     close(sock);
     exit (0);
   }
 
-  if(listen(sock,5)<0){
+  if(listen(sock,5)<0) {
     perror("listen");
     close(sock);
     exit (0);
   }
 
-  while(1){
+  while(1) {
 
-    if((s=accept(sock, NULL, 0))<0){
-      perror("accept");
-      close(sock);
-      close(s);
-      exit (0);
+    if((s=accept(sock, NULL, 0))<0) {
+            perror("accept");
+            close(sock);
+            close(s);
+            exit (0);
     }
 
     /*fork a child to handle the connection*/
 
-    if((pid=fork())){
-      close(s);
+    if((pid=fork())) {
+            close(s);
     }
     else {
       /*Child code*/
@@ -238,39 +221,41 @@ int main(int argc, char **argv)
       SSL_set_bio(ssl,sbio,sbio);
 
       if((r=SSL_accept(ssl)<=0))
-        berr_exit("SSL accept error");
+        bio_error_exit("SSL accept error");
 
+      /* check the client certificate */  
       check_cert(ssl);
 
       int len;
       char buf[256];
       char *answer = "42";
 
+      /* read the secret question from client */  
       r = SSL_read(ssl, &buf, 255);
-      switch(SSL_get_error(ssl,r)){
+      switch(SSL_get_error(ssl,r)) {
+      case SSL_ERROR_NONE:
+        len=r;
+        buf[len]= '\0';
+
+        /* print out and send the answer to client */  
+        printf(FMT_OUTPUT, buf, answer);
+        r = SSL_write(ssl, answer, strlen(answer));
+        switch(SSL_get_error(ssl,r)) {
         case SSL_ERROR_NONE:
-          len=r;
-          buf[len]= '\0';
-
-          printf(FMT_OUTPUT, buf, answer);
-          r = SSL_write(ssl, answer, strlen(answer));
-          switch(SSL_get_error(ssl,r)){
-          case SSL_ERROR_NONE:
-            if(strlen(answer)!=r)
-              err_exit("Incomplete write");
-            break;
-            default:
-              berr_exit("SSL write problem");
-          }
+          if(strlen(answer)!=r)
+                  error_exit("Incomplete write");
           break;
-        case SSL_ERROR_ZERO_RETURN:
-          break;
-        case SSL_ERROR_SYSCALL:
-          err_exit(FMT_INCOMPLETE_CLOSE);
         default:
-          berr_exit("SSL read problem");
+          bio_error_exit("SSL write problem");
+        }
+        break;
+      case SSL_ERROR_ZERO_RETURN:
+        break;
+      case SSL_ERROR_SYSCALL:
+        error_exit(FMT_INCOMPLETE_CLOSE);
+      default:
+        bio_error_exit("SSL read problem");
       }
-
 
       shutdown_server(ssl, s);
 
@@ -282,6 +267,6 @@ int main(int argc, char **argv)
   }
 
   close(sock);
-  destroy_ctx(ctx);
+  SSL_CTX_free(ctx);
   return 1;
 }
